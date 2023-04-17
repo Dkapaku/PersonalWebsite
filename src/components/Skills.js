0213a86fdff95a6428e076b9b8a758dee9271915
David Kapaku <dkapaku1@vt.edu>
2023-04-17 19:03:24 -0400

    touch up

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,26 @@
 const Skills=()=>{
     return(
         <div className="skillsTab fade-in">
-            <div>
-                <label for="skill_1">Foreign Language:</label> 
-                <meter id="skill_1" value="9.5" min="0" max="10"></meter>
-            </div>
+
             <div>
                 <label for="skill_2">HTML5/CSS3:</label>
                 <meter id="skill_2" value="9"min="0"max="10"></meter>
             </div>
             <div>
-                <label for="skill_3">Vanilla JS</label>
+                <label for="skill_3">Vanilla JS:</label>
                 <meter id="skill_3" value="9"min="0"max="10"></meter>
             </div>
             <div>
-                <label for="skill_4">React.js</label>
+                <label for="skill_4">React.js:</label>
                 <meter id="skill_4"value="8.5"min="0"max="10"></meter>
             </div>
             <div>
-                <label for="skill_5">Node.js</label>
+                <label for="skill_5">Node.js/Express:</label>
                 <meter id="skill_5"value="7"min="0"max="10"></meter>
+            </div>
+            <div>
+                <label for="skill_1">Foreign Language:</label> 
+                <meter id="skill_1" value="9.5" min="0" max="10"></meter>
             </div>
             <div>
                 <label for="skill_6">Interpersonal Skills</label>
